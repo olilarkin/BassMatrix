d91e6823939b59fc377295eb6470970cf556aa0f
Ola Wistedt <ola@witech.se>
2022-01-22 23:31:25 +0000

    Added DSP to web project.

--- BassMatrix/config/BassMatrix-web.mk
+++ BassMatrix/config/BassMatrix-web.mk
@@ -5,6 +5,25 @@ IPLUG2_ROOT = ../../iPlug2
 include ../../common-web.mk
 
 SRC += $(PROJECT_ROOT)/BassMatrix.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/GlobalFunctions.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_AcidPattern.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_AcidSequencer.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_AnalogEnvelope.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_BiquadFilter.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_BlendOscillator.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_Complex.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_DecayEnvelope.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_EllipticQuarterBandFilter.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_FourierTransformerRadix2.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_FunctionTemplates.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_LeakyIntegrator.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_MidiNoteEvent.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_MipMappedWaveTable.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_NumberManipulations.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_OnePoleFilter.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_Open303.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_RealFunctions.cpp
+SRC += $(PROJECT_ROOT)/open303/Source/DSPCode/rosic_TeeBeeFilter.cpp
 
 # WAM_SRC += 
 
